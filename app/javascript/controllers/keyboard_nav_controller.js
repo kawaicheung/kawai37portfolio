8dff699272ddc7e30534263a188d0157e9caf69a
Ka Wai Cheung <kawai.cheung@donedone.com>
2025-03-10 22:38:32 -0700

    More css

--- app/javascript/controllers/keyboard_nav_controller.js
+++ app/javascript/controllers/keyboard_nav_controller.js
@@ -1,7 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["instructions"]
   static values = {
     key: String,
     path: String
