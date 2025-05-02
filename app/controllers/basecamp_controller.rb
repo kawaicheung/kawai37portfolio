e3b2847fd96a2aae593f1288905401668adc07ca
Ka Wai Cheung <kawai.cheung@donedone.com>
2025-05-02 15:01:55 -0700

    bem work

--- app/controllers/basecamp_controller.rb
+++ app/controllers/basecamp_controller.rb
@@ -1,12 +1,10 @@
 class BasecampController < ApplicationController
   layout "parent_basecamp"
   def first_time
-    @my_title = "First Time"
     render "index"
   end
 
   def second_time
-    @my_title = "Second Time"
     render "index"
   end
 end
